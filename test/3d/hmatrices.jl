44582b938c0e209a8a26c99bee6fbc6fdf694a98
Mikkel Paltorp Schmitt <mpasc@elektro.dtu.dk>
2023-02-19 18:17:18 +0100

    Draft of possible tests
    For now the test is not used. The results are seemingly not deterministic. Needs a fix somehow.

--- test/3d/hmatrices.jl
+++ test/3d/hmatrices.jl
@@ -8,7 +8,7 @@ using IterativeSolvers
 #==========================================================================================
                             Testing mesh interpolation schemes
 ==========================================================================================#
-# Hmm, the linear physics fails. Here, but not if run seperately
+# Its not determistic
 @testset "Triangular Mesh" begin
     mesh_file = "../examples/meshes/sphere_1m"
     geometry_orders = [:linear,:quadratic]
@@ -24,28 +24,29 @@ using IterativeSolvers
         x = ones(size(HG,2))
         y = HG*x
         xg = gmres(HG,y)
+        xg = gmres(HG,y)
         println(go)
         println(po)
         @test norm(xg - x)/norm(x) ≈ 0 atol=1e-4
     end
 end
 
-# mesh_file = "../examples/meshes/sphere_1m"
-# geometry_orders = [:linear,:quadratic]
-# physics_orders  = [:linear,:quadratic,:disctriconstant,:disctrilinear,:disctriquadratic]
-# go = geometry_orders[1]
-# po = physics_orders[1]
-# # for go in geometry_orders, po in physics_orders
-#     # if go == :linear && po == :quadratic
-#         # continue
-#     # end
-#     mesh = load3dTriangularComsolMesh(mesh_file;geometry_order=go,physics_order=po)
-#     # TODO
-# # end
-# k = 1.0
-# HG = BoundaryIntegralEquations.HGOperator(mesh,k)
-# x = ones(size(HG,2))
-# y = HG*x
-# xg = gmres(HG,y)
+mesh_file = "../examples/meshes/sphere_1m"
+geometry_orders = [:linear,:quadratic]
+physics_orders  = [:linear,:quadratic,:disctriconstant,:disctrilinear,:disctriquadratic]
+go = geometry_orders[2]
+po = physics_orders[1]
+# for go in geometry_orders, po in physics_orders
+    # if go == :linear && po == :quadratic
+        # continue
+    # end
+    mesh = load3dTriangularComsolMesh(mesh_file;geometry_order=go,physics_order=po)
+    # TODO
+# end
+k = 1.0
+HG = BoundaryIntegralEquations.HGOperator(mesh,k)
+x = ones(size(HG,2))
+y = HG*x
+xg = gmres(HG,y)
 
-# @test norm(xg - x)/norm(x) ≈ 0 atol=1e-4
+@test norm(xg - x)/norm(x) ≈ 0 atol=1e-4
