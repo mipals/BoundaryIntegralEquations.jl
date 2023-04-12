ffc9aae3d81ad3009d24f7eed3628fdabc578066
mipals <mpasc@elektro.dtu.dk>
2023-04-12 13:42:35 +0200

    Fixing wrong 10x10 assembly

--- examples/paper1_comparisons.jl
+++ examples/paper1_comparisons.jl
@@ -11,126 +11,131 @@ using JLD2
 ==========================================================================================#
 # Triangular Meshes
 mesh_path = joinpath(dirname(pathof(BoundaryIntegralEquations)),"..","examples","meshes")
-mesh_file = "sphere_1m_coarser"
-mesh_file = "sphere_1m_coarse"
-# mesh_file = "sphere_1m"
-# mesh_file = "sphere_1m_fine"
-# mesh_file = "sphere_1m_finer"
-tri_mesh_file = joinpath(mesh_path,mesh_file);
-@time mesh = load3dTriangularComsolMesh(tri_mesh_file)
-#==========================================================================================
-                                    3d Visualization
-==========================================================================================#
-# using MeshViz
-# import WGLMakie as wgl
-# wgl.set_theme!(resolution=(800, 800))
-# simple_mesh = create_simple_mesh(mesh)
-# viz(simple_mesh, showfacets = true)
-#==========================================================================================
-                                Setting up constants
-==========================================================================================#
-freq   = 100.0                                   # Frequency                 [Hz]
-rho,c,kp,ka,kh,kv,ta,th,phi_a,phi_h,eta,mu = visco_thermal_constants(;freq=freq,S=1)
-k      = 2*π*freq/c                              # Wavenumber                [1/m]
-radius = 1.0                                     # Radius of sphere_1m       [m]
-ω = 2π*freq
-depth = 1
-#===========================================================================================
-                        Computing Condition Numbers
-===========================================================================================#
-if Sys.isapple()
-    data_directory = "/Users/mpasc/OneDrive - Danmarks Tekniske Universitet/paper1"
-else if Sys.islinux()
+# mesh_files = ["sphere_1m_coarser","sphere_1m_coarse","sphere_1m","sphere_1m_fine","sphere_1m_finer"]
+# mesh_files = ["sphere_1m_coarser","sphere_1m_coarse","sphere_1m","sphere_1m_fine"]
+mesh_files = ["sphere_1m_coarser","sphere_1m_coarse","sphere_1m"] # Just as a start
 
-end
-data_file = joinpath(data_directory, mesh_file, "condition_numbers_depth$(depth).jld2")
-if !isfile(data_file)
-    # Computing matrices
-    LGM = LossyGlobalOuter(mesh,freq;fmm_on=false,depth=depth);
-    # Creating the different systems
-    F10 = BoundaryIntegralEquations._full10(LGM)
-    F4  = BoundaryIntegralEquations._full4(LGM)
-    F1  = BoundaryIntegralEquations._full1(LGM)
-    #
-    @info "Computing Conditon Number of 1x1"
-    condF1  = cond(F1)
-    @info "Computing Conditon Number of 4x4"
-    condF4  = cond(F4)
-    @info "Computing Conditon Number of 10x10"
-    condF10 = cond(F10)
+# mesh_files = ["sphere_1m_coarser"]
 
-    jldsave(data_file, condF1=condF1,condF4=condF4,condF10=condF10)
+for mesh_file in mesh_files
 
-    # Plottin for a quick sanity check
-    K=1
-    gr(size=(600,300))
-    scatter(ang_axis[1:K:end],real.(pa[1:K:end]),label="BEM-global",marker=:cross,
-            markersize=2,color=:black,dpi=600,xtickfontsize=15,ytickfontsize=15,
-            legendfontsize=15,titlefontsize=15,xlabelfontsize=15,ylabelfontsize=15)
-    ylabel!("Re(p)"); plot!(ang_axis[perm],real.(pasAN[perm]),label="Analytical",linewidth=2)
-    title!("Frequency = $(freq)")
-    xlabel!("Angle")
-    savefig(joinpath(data_directory, mesh_file,"pa.png"))
-else
-    @warn "Loading data "
-    f = jldopen(dataFile)
-    condF1 = f["condF1"]
-    condF4 = f["condF4"]
-    condF10 = f["condF10"]
-end
+    tri_mesh_file = joinpath(mesh_path,mesh_file);
+    @time mesh = load3dTriangularComsolMesh(tri_mesh_file)
+    #==========================================================================================
+                                    Setting up constants
+    ==========================================================================================#
+    freq   = 100.0                                   # Frequency                 [Hz]
+    rho,c,kp,ka,kh,kv,ta,th,phi_a,phi_h,eta,mu = visco_thermal_constants(;freq=freq,S=1)
+    k      = 2*π*freq/c                              # Wavenumber                [1/m]
+    radius = 1.0                                     # Radius of sphere_1m       [m]
+    ω = 2π*freq
+    depth = 1
 
-
-#* Generating analytical solution
-pasAN, v_rAN, v_thetaAN, v_rAN_A, v_thetaAN_A, v_rAN_V, v_thetaAN_V =
+    xyzb = mesh.sources
+    n = size(xyzb,2)
+    u₀ = 1e-2
+    normals  = mesh.normals
+    ρ,c,kₚ,kₐ,kₕ,kᵥ,τₐ,τₕ,ϕₐ,ϕₕ,η,μ = visco_thermal_constants(;freq=freq,S=1)
+    #===========================================================================================
+                            Computing Condition Numbers
+    ===========================================================================================#
+    if Sys.isapple()
+        data_directory = "/Users/mpasc/OneDrive - Danmarks Tekniske Universitet/paper1"
+    elseif Sys.islinux()
+        data_directory = "/home/mpasc/OneDrive/paper1"
+    end
+        for depth in [1]
+        data_file = joinpath(data_directory, mesh_file, "results_depth$(depth)_freq$(Int(freq)).jld2")
+        if !isfile(data_file)
+            @info "Computing setup: $(data_file)"
+            # Computing matrices
+            LGM = LossyGlobalOuter(mesh,freq;fmm_on=false,depth=depth);
+            # Creating the different systems
+            F10 = BoundaryIntegralEquations._full10(LGM)
+            F4  = BoundaryIntegralEquations._full4(LGM)
+            F1  = BoundaryIntegralEquations._full1(LGM)
+            #
+            @info "Computing Conditon Number of 1x1 (n=$(n))"
+            condF1  = cond(F1)
+            @info "Computing Conditon Number of 4x4 (n=$(n))"
+            condF4  = cond(F4)
+            @info "Computing Conditon Number of 10x10 (n=$(n))"
+            condF10 = cond(F10)
+            
+            # Plottin for a quick sanity check
+            v0 = [u₀*ones(n); zeros(2n)]
+            coordinates = [radius*ones(n,1) acos.(xyzb[1,:]/radius)]
+            rhs = LGM.Ga*gmres(LGM.inner,LGM.Dr*v0 - LGM.Nd'*gmres(LGM.Gv,LGM.Hv*v0);verbose=true);
+            pasAN, v_rAN, v_thetaAN, v_rAN_A, v_thetaAN_A, v_rAN_V, v_thetaAN_V =
                 BoundaryIntegralEquations.sphere_first_order(kₐ,c,ρ,radius,u₀,coordinates;S=1,kv=kᵥ)
-ang_axis = coordinates[:,2]*180.0/pi
-perm = sortperm(ang_axis)
+            ang_axis = coordinates[:,2]*180.0/pi
+            sol1  = F1\rhs
+            sol4  = F4\[zeros(n); v0]
+            sol10 = F10\[zeros(7n);v0]
+            # Extracting pressures
+            pa1  = sol1
+            pa4  = sol4[1:n]
+            pa10 = sol10[1:n]
 
-# Plotting
-K = 1
-gr(size=(600,300))
-scatter(ang_axis[1:K:end],real.(pa[1:K:end]),label="BEM-global",marker=:cross,
-        markersize=2,color=:black,dpi=600,xtickfontsize=15,ytickfontsize=15,
-        legendfontsize=15,titlefontsize=15,xlabelfontsize=15,ylabelfontsize=15)
-ylabel!("Re(p)"); plot!(ang_axis[perm],real.(pasAN[perm]),label="Analytical",linewidth=2)
-title!("Frequency = $(freq)")
-xlabel!("Angle")
-# savefig("pa$(n).png")
-# savefig("/Users/mpasc/Dropbox/Apps/ShareLaTeX/JTCA_Iterative_losses/figures/notes/pa$(n).png")
-#===========================================================================================
-                                Checking results
-===========================================================================================#
-ph   = -LGM.tau_a/LGM.tau_h * pa
-dpa  = -gmres(LGM.Ga,LGM.Ha*pa;verbose=true) # <- This is the bottleneck...
-dph  = -gmres(LGM.Gh,LGM.Hh*ph)
-v   = v0 - (LGM.phi_a*LGM.Dc*pa  +
-            LGM.phi_a*LGM.Nd*dpa +
-            LGM.phi_h*LGM.Dc*ph  +
-            LGM.phi_h*LGM.Nd*dph)
-dvn = -gmres(LGM.Gv,LGM.Hv*v;verbose=true)
-#===========================================================================================
-                                   Plotting solutions
-===========================================================================================#
-vx = v[0n+1:1n]
-vy = v[1n+1:2n]
-vz = v[2n+1:3n]
-dvx = dvn[0n+1:1n]
-dvy = dvn[1n+1:2n]
-dvz = dvn[2n+1:3n]
-#* Normal compontent of the viscous flow
-v_n0   = LGM.Nd'*v
-#* Computing the tangential velocity by substracting the normal information
-v_t = v + LGM.Nd*v_n0
-vt_sum = sqrt.(v_t[0n+1:1n].^2 + v_t[1n+1:2n].^2 + v_t[2n+1:3n].^2)
-#! Skipping points. Useful when the mesh is large
-K = 1
-plt1 = scatter(ang_axis[1:K:end],real.(pa[1:K:end]),label="BEM",markersize=2)
-plot!(ang_axis[perm],real.(pasAN[perm]),label="Analytical",linewidth=2)
-ylabel!("Re(p)");  title!("Frequency = $(freq)")
-plt2 = scatter(ang_axis[1:K:end],real.(v_n0[1:K:end]),label=false,markersize=2)
-plot!(ang_axis[perm],real.(v_rAN_V[perm]),label=false,linewidth=2)
-ylabel!("Re(Vn)")
-plt3 = scatter(ang_axis[1:K:end],real.(vt_sum[1:K:end]),label=false,markersize=2)
-plot!(ang_axis[perm],real.(v_thetaAN_V[perm]),label=false,linewidth=2)
-xlabel!("Angle"); ylabel!("Re(Vt)")
-plot(plt1,plt2,plt3,layout=(3,1),dpi=500)
+            perm = sortperm(ang_axis)
+            gr(size=(600,300))
+            scatter(ang_axis,real.(pa1),label="BEM-1",marker=:square,
+                    markersize=2,dpi=600,xtickfontsize=15,ytickfontsize=15,
+                    legendfontsize=15,titlefontsize=15,xlabelfontsize=15,ylabelfontsize=15)
+            scatter!(ang_axis,real.(pa4),label="BEM-4",marker=:triangle,
+                    markersize=2,xtickfontsize=15,ytickfontsize=15,
+                    legendfontsize=15,titlefontsize=15,xlabelfontsize=15,ylabelfontsize=15)
+            scatter!(ang_axis,real.(pa10),label="BEM-10",marker=:cross,
+                    markersize=2,xtickfontsize=15,ytickfontsize=15,
+                    legendfontsize=15,titlefontsize=15,xlabelfontsize=15,ylabelfontsize=15)
+            ylabel!("Re(p)"); plot!(ang_axis[perm],real.(pasAN[perm]),label="Analytical",linewidth=2)
+            title!("Frequency = $(freq)"); xlabel!("Angle")
+            savefig(joinpath(data_directory, mesh_file,"all_pa_depth$(depth)_freq$(Int(freq)).png"))
+
+            # Computing normal velocity
+            Aview = @view F4[1n+1:4n,0n+1:1n] # Because I am lazy i use the bottom equation from F4 directly
+            v1  = v0 - Aview*pa1    # This is the bottom equation of F4
+            v4  = sol4[1n+1:4n]
+            v10 = sol10[4n+1:7n]
+
+            v_n1  = LGM.Nd'*v1
+            v_n4  = LGM.Nd'*v4
+            v_n10 = LGM.Nd'*v10
+
+            scatter(ang_axis,real.(v_n1),label="BEM-1",marker=:square,
+                markersize=2,dpi=600,xtickfontsize=15,ytickfontsize=15,
+                legendfontsize=15,titlefontsize=15,xlabelfontsize=15,ylabelfontsize=15)
+            scatter!(ang_axis,real.(v_n4),label="BEM-4",marker=:triangle,
+                markersize=2,xtickfontsize=15,ytickfontsize=15,
+                legendfontsize=15,titlefontsize=15,xlabelfontsize=15,ylabelfontsize=15)
+            scatter!(ang_axis,real.(v_n10),label="BEM-10",marker=:cross,
+                markersize=2,xtickfontsize=15,ytickfontsize=15,
+                legendfontsize=15,titlefontsize=15,xlabelfontsize=15,ylabelfontsize=15)
+            ylabel!("Re(p)"); plot!(ang_axis[perm],real.(v_rAN_V[perm]),label="Analytical",linewidth=2)
+            title!("Frequency = $(freq)"); xlabel!("Angle")
+            savefig(joinpath(data_directory, mesh_file,"all_vn_depth$(depth)_freq$(Int(freq)).png"))
+
+            # Saving data
+            jldsave(data_file, 
+                    condF1=condF1,
+                    condF4=condF4,
+                    condF10=condF1,
+                    pa1=pa1,
+                    pa4=pa4,
+                    pa10=pa10,
+                    v_n1=v_n1,
+                    v_n4=v_n4,
+                    v_n10=v_n10,
+                    ang_axis=ang_axis,
+                    pasAN=pasAN
+                    )
+        else
+            @warn "Loading data "
+            f = jldopen(data_file)
+            condF1 = f["condF1"]
+            condF4 = f["condF4"]
+            condF10 = f["condF10"]
+            close(f)
+        end
+    end
+end
--- src/3d/LossyGlobalMatrix.jl
+++ src/3d/LossyGlobalMatrix.jl
@@ -202,26 +202,26 @@ function _full10(A::LossyGlobalOuter)
     F = zeros(eltype(A), 10n,10n)
 
     # Acoustical Mode
-    F[0n+1:1n,0n+1:1n] = A.Ga
-    F[0n+1:1n,1n+1:2n] = A.Ha
+    F[0n+1:1n,0n+1:1n] = A.Ha
+    F[0n+1:1n,1n+1:2n] = A.Ga
     # Thermal Mode
-    F[1n+1:2n,2n+1:3n] = A.Gh
-    F[1n+1:2n,3n+1:4n] = A.Hh
+    F[1n+1:2n,2n+1:3n] = A.Hh
+    F[1n+1:2n,3n+1:4n] = A.Gh
     # Viscous Mode
-    F[2n+1:5n,4n+1:7n]  = A.Gv
-    F[2n+1:5n,7n+1:10n] = A.Hv
+    F[2n+1:5n,4n+1:7n]  = A.Hv
+    F[2n+1:5n,7n+1:10n] = A.Gv
     # Divergence
     F[5n+1:6n,4n+1:7n] = A.Dr
     F[5n+1:6n,7n+1:10n] = A.Nd'
     # Isothermal BC
-    F[6n+1:7n,0n+1:1n] = A.tau_a*Diagonal(ones(n))
-    F[6n+1:7n,2n+1:3n] = A.tau_h*Diagonal(ones(n))
+    F[6n+1:7n,0n+1:1n] = A.tau_a*Diagonal(ones(eltype(A),n))
+    F[6n+1:7n,2n+1:3n] = A.tau_h*Diagonal(ones(eltype(A),n))
     # No-slip
     F[7n+1:10n,0n+1:1n] = A.phi_a*A.Dc
     F[7n+1:10n,1n+1:2n] = A.phi_a*A.Nd
     F[7n+1:10n,2n+1:3n] = A.phi_h*A.Dc
     F[7n+1:10n,3n+1:4n] = A.phi_h*A.Nd
-    F[7n+1:10n,4n+1:7n] = Diagonal(ones(3n))
+    F[7n+1:10n,4n+1:7n] = Diagonal(ones(eltype(A),3n))
 
     return F
 end
